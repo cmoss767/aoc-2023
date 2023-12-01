f94758d088031668409b0f54f6a60877fcb314c8
Chris Moss <hcmoss70@gmail.com>
2023-12-01 11:06:15 -0500

    part two

--- day1.py
+++ day1.py
@@ -1,18 +1,27 @@
+
+
 file = open("input.txt", "r").read().split()
 
 
+charToDigit = {"one": "1", "two": "2", "three": "3", "four": "4",
+               "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 sum = 0
-print(file)
+
 
 for line in file:
+    lines = []
+    for num in charToDigit.keys():
+        lines.append(line.replace(num, charToDigit[num]))
+    line = min(lines)
+
     first, firstNum, lastNum = False, '', ''
 
-    for i, c in enumerate(line):
-        if not first and c.isdigit():
-            firstNum = c
+    for char in line:
+        if not first and char.isdigit():
+            firstNum = char
             first = True
-        elif c.isdigit():
-            lastNum = c
+        elif char.isdigit():
+            lastNum = char
 
     if not lastNum:
         sum += int(firstNum + firstNum)
