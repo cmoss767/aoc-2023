0a170bc362e605575c78ceb9e26877deb3aa6f95
Chris Moss <hcmoss70@gmail.com>
2023-12-02 08:22:21 -0500

    think this is better

--- day1.py
+++ day1.py
@@ -1,18 +1,12 @@
-
+sum = 0
+charToDigit = {"one": "1", "two": "2", "three": "3", "four": "4",
+               "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 
 file = open("input.txt", "r").read().split()
 
-
-charToDigit = {"one": "1", "two": "2", "three": "3", "four": "4",
-               "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
-sum = 0
-
-
 for line in file:
-    lines = []
     for num in charToDigit.keys():
-        lines.append(line.replace(num, charToDigit[num]))
-    line = min(lines)
+        line = line.replace(num, charToDigit[num])
 
     first, firstNum, lastNum = False, '', ''
 
