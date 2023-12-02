c184af2e95c2a3cc924a15218baf42450213258a
Chris Moss <hcmoss70@gmail.com>
2023-12-02 15:15:03 -0500

    ok this is actually it

--- day1.py
+++ day1.py
@@ -1,13 +1,24 @@
-sum = 0
+res = 0
+file = open("input.txt", "r").read().split()
 charToDigit = {"one": "1", "two": "2", "three": "3", "four": "4",
                "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 
-file = open("input.txt", "r").read().split()
-
 for line in file:
+    # loop through all the char nums in charToDigit for every line
+    # l is lower index of each num
+    # r iterates through the line and will be the upper index of each num
+    # if num exists in the line string then replace the middle char of num with its digit
+    # by replacing the middle char this hopefully won't disrupt other nums that the substring is a part of
+    # example: zoneight234 -> zo1eight234 -> zo1ei8ht234
     for num in charToDigit.keys():
-        line = line.replace(num, charToDigit[num])
+        l = 0
+        for r in range(len(num) - 1, len(line)):
+            if line[l:r + 1] == num:
+                mid = (r + l) // 2
+                line = line[:mid] + charToDigit[num] + line[mid + 1:]
+            l += 1
 
+    # below code is the same as part1
     first, firstNum, lastNum = False, '', ''
 
     for char in line:
@@ -18,9 +29,8 @@ for line in file:
             lastNum = char
 
     if not lastNum:
-        sum += int(firstNum + firstNum)
+        res += int(firstNum + firstNum)
     else:
-        sum += int(firstNum + lastNum)
+        res += int(firstNum + lastNum)
 
-
-print(sum)
+print(res)
